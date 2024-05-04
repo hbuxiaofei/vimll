98ded9b2f3008ec766daf3a6f402ab4090e69881
rayylee <rayylee@foxmail.com>
2024-05-04 19:33:40 +0800

    update install.sh for sudo install
    Signed-off-by: rayylee <rayylee@foxmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+USRLOCAL_BIN=/usr/local/bin
+export PATH=$USRLOCAL_BIN:$PATH
+
+INSTALL_HOME=$HOME
+[ -n "$SUDO_USER" ] && INSTALL_HOME=/home/$SUDO_USER
+
 cd $(dirname $0)
 
 echo -e "\033[32m- [Info] Start to copy files\033[0m"
@@ -15,10 +21,10 @@ if [ -f /etc/vimrc ] && [ ! -f /etc/vimrc.bak ]; then
     touch /etc/vimrc
 fi
 
-rm -rf /$HOME/.vim >/dev/null 2>&1
-rm -f /$HOME/.vimrc >/dev/null 2>&1
-cp -rf vim /$HOME/.vim
-cp -f vimrc /$HOME/.vimrc
+rm -rf /${INSTALL_HOME}/.vim >/dev/null 2>&1
+rm -f /${INSTALL_HOME}/.vimrc >/dev/null 2>&1
+cp -rf vim /${INSTALL_HOME}/.vim
+cp -f vimrc /${INSTALL_HOME}/.vimrc
 
 
 echo -e "\033[32m- [Info] Install successfully...\033[0m"
